433c426392a9a0907721ebe93af43fae7480a430
JennyG28 <78384229+JennyG28@users.noreply.github.com>
2021-11-09 08:47:24 -0600

    Created Search Engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         Dictionary
       </header>
+      <main><Dictionary/></main>
+      <footer className="text-center">Coded by Jenny Gomez</footer>
+    </div>
     </div>
   );
 }
