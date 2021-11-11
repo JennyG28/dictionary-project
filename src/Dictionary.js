6049028b45b3439371c9c3ca9b5e69124b0bd8a5
JennyG28 <78384229+JennyG28@users.noreply.github.com>
2021-11-11 08:51:48 -0600

    Created component Meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary(){
 let [keyword, setKeyword] = useState ("");
+let [results, setResults] = useState(null);
 
 function handleResponse(response){
-
+setResults(response.data[0]);
 }
 
 function search(event){
   event.preventDefaul();
  
+  //documentation: https://dictionaryapi.dev/
 
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
@@ -21,11 +24,16 @@ axios.get(apiUrl).then(handleResponse);
 function handleKeywordChange(event){
   setKeyword(event.target.value);
 }
+
+ // <Results results={results}/> : component name , property name, value
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange}/>
       </form>
+      
+      <Results results={results}/>
+     
     </div>
   );
 }
