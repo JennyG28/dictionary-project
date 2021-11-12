5195ee696aa68f408dc8c14efb9bd076bfc3a5d6
JennyG28 <78384229+JennyG28@users.noreply.github.com>
2021-11-12 09:17:50 -0600

    Created Phonetics component

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetic(props){
+
+  return (
+    <div className="Phonetic">
+     {"/"}{props.phonetic.text}{"/"}{" "}   
+     <a href={props.phonetic.audio} target="_blank">
+        ► 
+      </a>
+    </div>
+  );
+}
