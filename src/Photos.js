08074edf526f65406f4aec1069bf42f5d4e68316
JennyG28 <78384229+JennyG28@users.noreply.github.com>
2021-11-18 09:11:13 -0600

    Change alt description

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,7 @@ export default function Photos(props){
         {props.photos.map(function (photo, index) {
           return (
           <div className="col-4" key={index}>
-          <img src={photo.src.medium} className="img-fluid" alt="photo" />
+          <img src={photo.src.medium} className="img-fluid" alt="logo" />
           </div>
           );
         })}
